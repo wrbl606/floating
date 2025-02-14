134b1533a8fe7a233c117af179f242e5bd4139a5
Thibault Deckers <thibault.deckers@gmail.com>
2025-02-14 20:15:32 +0100

    removed obsolete import (#33)

--- android/src/main/kotlin/eu/wroblewscy/marcin/floating/floating/FloatingPlugin.kt
+++ android/src/main/kotlin/eu/wroblewscy/marcin/floating/floating/FloatingPlugin.kt
@@ -16,7 +16,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.util.*
 import kotlin.concurrent.fixedRateTimer
 
